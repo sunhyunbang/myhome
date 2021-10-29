5117689b5e085ad23a31f8459e6e78ae2b4cc8d7
snhyun82 <0189875@naver.com>
2021-10-29 17:59:52 +0900

    21.10.29 add Del

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -46,5 +43,10 @@ public class BoardController {
         }
         boardRepository.save(board);
         return "redirect:/board/list";
+    }
+
+    @DeleteMapping("form_del")
+    public String formDel(){
+
     }
 }
